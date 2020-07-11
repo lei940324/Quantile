5aab668ebc022b8fa12fc90fa4a79972e975e96f
547222786@qq.com <547222786@qq.com>
2020-07-11 22:33:35 +0800

    调整代码格式

--- func.py
+++ func.py
@@ -15,13 +15,13 @@ import logging
 import threading
 import re
 
-EXIT = False   # 线程退出信号
+EXIT = False  # 线程退出信号
+
 
 class Quantile_Granger():
     """
     创建分位数Granger因果检验类
     """
-
     def set_range(self, start, end, num):
         '''
         生成估计区间
@@ -40,7 +40,9 @@ class Quantile_Granger():
         '''
         qrange = np.linspace(start, end, num)
         T = len(qrange)
-        qr_name = [f'[{qrange[i]:.2f}-{qrange[i+1]:.2f}]' for i in range(T-1)]
+        qr_name = [
+            f'[{qrange[i]:.2f}-{qrange[i+1]:.2f}]' for i in range(T - 1)
+        ]
         return qrange, qr_name
 
     def pattern(self, df, sign):
@@ -74,9 +76,9 @@ class Quantile_Granger():
                         Yname = df.columns.values[j]
                         DataList[f'{Xname}——>{Yname}'] = pd.DataFrame([X, Y]).T
         elif sign == '多因素对单市场':
-            Y = df.iloc[:, df.shape[1]-1]
-            Yname = df.columns.values[df.shape[1]-1]
-            for i in range(df.shape[1]-1):
+            Y = df.iloc[:, df.shape[1] - 1]
+            Yname = df.columns.values[df.shape[1] - 1]
+            for i in range(df.shape[1] - 1):
                 X = df.iloc[:, i]
                 Xname = df.columns.values[i]
                 DataList[f'{Xname}——>{Yname}'] = pd.DataFrame([X, Y]).T
@@ -101,12 +103,12 @@ class Quantile_Granger():
         data = pd.DataFrame()
         T = len(Y)
         data['y'] = list(Y[max(p, q):T])
-        for i in range(1, p+1):
+        for i in range(1, p + 1):
             name = f'y_{i}'
-            data[name] = list(Y[max(p, q)-i:T-i])
-        for i in range(1, q+1):
+            data[name] = list(Y[max(p, q) - i:T - i])
+        for i in range(1, q + 1):
             name = f'x_{i}'
-            data[name] = list(X[max(p, q)-i:T-i])
+            data[name] = list(X[max(p, q) - i:T - i])
         return data
 
     def qreg(self, data, Q):
@@ -134,7 +136,16 @@ class Quantile_Granger():
         # print(res.summary())
         return res
 
-    def calculate(self, DataList, qrange, qr_name, max_lag, info_type, WaldNum, sign_num, AicNum, objects=[logging.info]):
+    def calculate(self,
+                  DataList,
+                  qrange,
+                  qr_name,
+                  max_lag,
+                  info_type,
+                  WaldNum,
+                  sign_num,
+                  AicNum,
+                  objects=[logging.info]):
         """
         循环计算得到sup_wald值
 
@@ -158,11 +169,13 @@ class Quantile_Granger():
         global EXIT
         # 日志设定
         if logging.info in objects:
-            logging.basicConfig(filename=r'.\运行结果\运行细节.txt', level=logging.INFO,
-                                format='%(asctime)s - %(levelname)s - %(message)s')
+            logging.basicConfig(
+                filename=r'.\运行结果\运行细节.txt',
+                level=logging.INFO,
+                format='%(asctime)s - %(levelname)s - %(message)s')
         for object in objects:
             object('程序开始运行')
-        
+
         results = pd.DataFrame()
         for key in DataList:
             relation = key
@@ -173,10 +186,10 @@ class Quantile_Granger():
             LagQueue = Queue()
             Qspaces = []
             for i in range(AicNum):
-                Qspaces.append(i*0.9/(AicNum-1)+0.05)
+                Qspaces.append(i * 0.9 / (AicNum - 1) + 0.05)
             for Q in Qspaces:
-                for p in range(1, max_lag+1):
-                    for q in range(1, max_lag+1):
+                for p in range(1, max_lag + 1):
+                    for q in range(1, max_lag + 1):
                         LagQueue.put([Q, p, q])
 
             # 多线程计算信息准则,选取最优滞后阶数
@@ -190,21 +203,21 @@ class Quantile_Granger():
                         break
                     data = self.lag_list(Y, X, p, q)
                     res = self.qreg(data, Q)
-                    n = len(Y)-max(p, q)
+                    n = len(Y) - max(p, q)
                     ssr = []
                     for i in res.resid:
                         if i >= 0:
-                            ssr.append(Q*i)
+                            ssr.append(Q * i)
                         else:
-                            ssr.append((Q-1)*i)
-                    SSE = sum(ssr)/n
+                            ssr.append((Q - 1) * i)
+                    SSE = sum(ssr) / n
                     L = math.log(SSE)
-                    k = p+q+1
+                    k = p + q + 1
                     if info_type == 'AIC':
-                        AIC = L+k/n
+                        AIC = L + k / n
                         NAME = 'AIC'
                     else:
-                        AIC = L+(math.log(n)*k)/(2*n)
+                        AIC = L + (math.log(n) * k) / (2 * n)
                         NAME = 'BIC'
                     if Q in self.AicDict:
                         self.AicDict[Q].append([p, q, AIC])
@@ -212,7 +225,8 @@ class Quantile_Granger():
                         self.AicDict[Q] = [[p, q, AIC]]
                     for object in objects:
                         object(
-                            f'正在进行{relation},分位点:{Q:.2f}的{NAME}计算,滞后阶数为[{p},{q}],{NAME}值为{AIC:.2f}')
+                            f'正在进行{relation},分位点:{Q:.2f}的{NAME}计算,滞后阶数为[{p},{q}],{NAME}值为{AIC:.2f}'
+                        )
 
             threadCrawl = []
             for i in range(10):
@@ -225,7 +239,7 @@ class Quantile_Granger():
                 object(f"计算{relation}最优滞后阶数线程退出循环")
 
             # 选取最优阶数
-            if EXIT: 
+            if EXIT:
                 for object in objects:
                     object(f"程序已终止运行")
                 return 0
@@ -233,7 +247,7 @@ class Quantile_Granger():
             for i in range(len(qr_name)):
                 QAICS = []
                 for Q in self.AicDict:
-                    if qrange[i] <= Q <= qrange[i+1]:
+                    if qrange[i] <= Q <= qrange[i + 1]:
                         for QAIC in self.AicDict[Q]:
                             QAICS.append(QAIC)
                 QAICS = sorted(QAICS, key=lambda x: x[2])
@@ -243,12 +257,12 @@ class Quantile_Granger():
             QregQueue = Queue()
             Qs = []
             for i in range(WaldNum):
-                Qs.append(i*0.9/(WaldNum-1)+0.05)
-            for i in range(len(qrange)-1):
+                Qs.append(i * 0.9 / (WaldNum - 1) + 0.05)
+            for i in range(len(qrange) - 1):
                 for Q in Qs:
-                    if qrange[i] <= Q <= qrange[i+1]:
-                        QregQueue.put([Q]+LagDict[qr_name[i]]+[i])
-            
+                    if qrange[i] <= Q <= qrange[i + 1]:
+                        QregQueue.put([Q] + LagDict[qr_name[i]] + [i])
+
             # 11.多线程计算wald值
             self.WaldDict = {}
 
@@ -270,7 +284,8 @@ class Quantile_Granger():
                     self.WaldDict[(Q, index)] = [p, q, walds]
                     for object in objects:
                         object(
-                            f'正在进行{relation},分位区间:{qr_name[index]},分位点:{Q:.2f}的wald值,滞后阶数为[{p},{q}],wald值为{walds:.2f}')
+                            f'正在进行{relation},分位区间:{qr_name[index]},分位点:{Q:.2f}的wald值,滞后阶数为[{p},{q}],wald值为{walds:.2f}'
+                        )
 
             threadCrawl = []
             for i in range(10):
@@ -283,12 +298,12 @@ class Quantile_Granger():
                 object(f"计算{relation}的wald线程退出循环")
 
             # 12.计算Sup-Wald值
-            if EXIT: 
+            if EXIT:
                 for object in objects:
                     object(f"程序已终止运行")
                 return 0
             SupDict = {}
-            for i in range(len(qrange)-1):
+            for i in range(len(qrange) - 1):
                 SUP = []
                 for Q in self.WaldDict:
                     if Q[1] == i:
@@ -300,9 +315,9 @@ class Quantile_Granger():
             Swl = pd.read_excel(r'.\data\Sup_wald_lag.xlsx')
             tao = []
             for i in range(len(qr_name)):
-                fenzi = qrange[i+1]*(1-qrange[i])
-                fenmu = qrange[i]*(1-qrange[i+1])
-                tao.append(fenzi/fenmu)
+                fenzi = qrange[i + 1] * (1 - qrange[i])
+                fenmu = qrange[i] * (1 - qrange[i + 1])
+                tao.append(fenzi / fenmu)
             x = Swl[0]
             y = Swl.drop(0, axis=1)
             # 插值拟合
@@ -346,19 +361,19 @@ if __name__ == "__main__":  # 用于当前窗体测试
     df = df.drop(df.columns[0], axis=1)
 
     # 设定参数
-    start = 0.1   # 区间起点
-    end = 0.9   # 区间终点
-    num = 17   # 区间个数
-    sign = '单因素对各市场'   # 模式选择
-    max_lag = 1   # 最大滞后阶数选择，默认为5
-    info_type = 'BIC'   # 信息准则选择
-    WaldNum = 35   # 估计wald个数，默认1000个
-    sign_num = 2   # 有效数字，默认为3
-    AicNum = 20   # 估计各区间最优滞后阶数，默认50个
+    start = 0.1  # 区间起点
+    end = 0.9  # 区间终点
+    num = 17  # 区间个数
+    sign = '单因素对各市场'  # 模式选择
+    max_lag = 1  # 最大滞后阶数选择，默认为5
+    info_type = 'BIC'  # 信息准则选择
+    WaldNum = 35  # 估计wald个数，默认1000个
+    sign_num = 2  # 有效数字，默认为3
+    AicNum = 20  # 估计各区间最优滞后阶数，默认50个
 
     # 开始计算
     qrange, qr_name = ex.set_range(start, end, num)
     DataList = ex.pattern(df, sign)
-    results = ex.calculate(DataList, qrange, qr_name,
-                           max_lag, info_type, WaldNum, sign_num, AicNum)
+    results = ex.calculate(DataList, qrange, qr_name, max_lag, info_type,
+                           WaldNum, sign_num, AicNum)
     results.to_excel('./运行结果/Granger.xlsx')
